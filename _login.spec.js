34a8985074bf801268ad027286abb86f17650821
Kabidu Munguakonkwa Sage <107588853+Abidusage@users.noreply.github.com>
2026-06-20 20:25:22 +0100

    #1: Implement E2E Tests for Login Use Case

--- _login.spec.js
+++ _login.spec.js
@@ -6,15 +6,38 @@ const { chromium } = require('playwright');
   });
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto('https://student.michaelkentburns.com/');
-  await page.getByRole('link', { name: 'User' }).click();
-  await page.getByRole('link', { name: 'Login' }).click();
+  await page.goto('https://student.michaelkentburns.com/wp-login.php?action=register');
+  // await expect(page.getByRole('heading', { name: 'Registration Form' })).toBeVisible();
+
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('kasage');
+  await page.getByRole('textbox', { name: 'Username' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Email' }).fill('kasageabidu@gmail.com');
+  await page.getByRole('button', { name: 'Register' }).click();
+  // await expect(page.getByRole('textbox', { name: 'Username' })).toBeVisible();
+
+  await page.locator('#login_error').getByRole('link', { name: 'Log in' }).click();
+  // await expect(page.getByRole('button', { name: 'Show password' })).toBeVisible();
+
   await page.getByRole('textbox', { name: 'Username or Email Address' }).click();
-  await page.getByRole('textbox', { name: 'Username or Email Address' }).fill('kabidusage');
+  await page.getByRole('textbox', { name: 'Username or Email Address' }).fill('kasage');
+  await page.getByRole('textbox', { name: 'Username or Email Address' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('Toujours#toucher*');
+  await page.getByRole('button', { name: 'Show password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).click();
-  const page1 = await context.newPage();
-  await page1.close();
-  await page.close();
+
+  await page.getByText('Remember Me').click();
+  await page.getByRole('button', { name: 'Log In' }).click();
+  // await expect(page.getByRole('region', { name: 'We value your privacy' })).toBeVisible();
+
+  await page.getByRole('link', { name: 'Home' }).click();
+  // await expect(page.getByRole('region', { name: 'We value your privacy' })).toBeVisible();
+
+  await page.getByRole('link', { name: 'About' }).click();
+  // await expect(page.getByRole('region', { name: 'We value your privacy' })).toBeVisible();
+
+  await page.getByRole('link', { name: 'Student' }).click();
+  await page.getByRole('link', { name: 'Logout' }).click();
 
   // ---------------------
   await context.close();
