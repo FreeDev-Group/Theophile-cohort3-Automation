032b955e77eaf4fa2c7aac30714c6e8990575143
Guillain Wise <guillainwise@gmail.com>
2026-07-06 12:46:29 +0200

    #1: End to End Login

--- tests/wise/login.spec.js
+++ tests/wise/login.spec.js
@@ -1,57 +1,52 @@
-const test = require('node:test');
-const { chromium } = require('playwright');
+import { test, expect } from '@playwright/test';
 
-test('Register Test',  async () => {
-  const browser = await chromium.launch({
-    headless: false
-  });
-  const context = await browser.newContext();
-  const page = await context.newPage();
+test('login', async ({ page }) => {
   await page.goto('https://student.michaelkentburns.com/');
+  await expect(page.getByRole('region', { name: 'We value your privacy' })).toBeVisible();
+
   await page.getByRole('link', { name: 'User' }).click();
-  await page.getByRole('link', { name: 'Register as Student' }).click();
-  await page.getByRole('textbox', { name: 'Username' }).fill('emsautoservices5@gmail.com');
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Username or Email Address' }).fill('emsautoservices5@gmail.com');
+  await page.getByRole('textbox', { name: 'Username or Email Address' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('|*Gui_llain@97');
+  await page.getByRole('button', { name: 'Log In' }).click();
+  await page.pause();
+});
+test.only('create-account', async ({ page }) => {
+  await page.goto('https://student.michaelkentburns.com/wp-login.php?action=register');
+  await page.getByRole('textbox', { name: 'Username' }).fill('EMS AUTO');
   await page.getByRole('textbox', { name: 'Username' }).press('Tab');
-  await page.getByRole('textbox', { name: 'Email' }).fill('|*Gui_llain@97');
+  await page.getByRole('textbox', { name: 'Email' }).fill('emsautoservices5@gmail.com');
   await page.getByRole('button', { name: 'Register' }).click();
 });
 
-test('Login Test', async ({page}) => {
- await page.goto('https://student.michaelkentburns.com/wp-login.php');
-  // await expect(page.getByRole('button', { name: 'Show password' })).toBeVisible();
+test.only('reset-password-to-login', async ({ page }) => {
+  await page.goto('https://student.michaelkentburns.com/wp-login.php?action=rp');
+  await page.getByRole('textbox', { name: 'New password', exact: true }).fill('|*Gui_llain@97');
+  await page.getByRole('button', { name: 'Save Password' }).click();
+});
 
-  await page.getByRole('textbox', { name: 'Username or Email Address' }).click();
-    await page.getByRole('textbox', { name: 'Username or Email Address' }).fill('emsautoservices5@gmail.com');
-    await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('|*Gui_llain@97');
-  await page.getByText('Log In Powered by WordPress').click();
-  await page.getByRole('checkbox', { name: 'Remember Me' }).check();
-  await page.getByRole('button', { name: 'Log In' }).click();
+test.only('login', async ({ page }) => {
   await page.goto('https://student.michaelkentburns.com/');
-  // await expect(page.getByRole('region', { name: 'We value your privacy' })).toBeVisible();
+  await expect(page.getByRole('region', { name: 'We value your privacy' })).toBeVisible();
 
-  await page.getByRole('banner').click();
-  await page.getByRole('link', { name: 'Home' }).click();
-  // await expect(page.getByRole('region', { name: 'We value your privacy' })).toBeVisible();
-
-  await page.getByRole('link', { name: 'About' }).click();
-  // await expect(page.getByRole('region', { name: 'We value your privacy' })).toBeVisible();
-
-  await page.getByRole('link', { name: 'All Surveys' }).click();
-  await page.goto('https://student.michaelkentburns.com/about/');
-  // await expect(page.getByRole('region', { name: 'We value your privacy' })).toBeVisible();
+  await page.getByRole('link', { name: 'User' }).click();
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Username or Email Address' }).fill('emsautoservices5@gmail.com');
+  await page.getByRole('textbox', { name: 'Username or Email Address' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('|*Gui_llain@97');
+  await page.getByRole('button', { name: 'Log In' }).click();
+  await expect(page.getByRole('region', { name: 'We value your privacy' })).toBeVisible();
 
   await page.getByRole('link', { name: 'My Completed Surveys' }).click();
-  // await expect(page.getByRole('region', { name: 'We value your privacy' })).toBeVisible();
+  await expect(page.getByRole('region', { name: 'We value your privacy' })).toBeVisible();
 
-  await page.getByRole('link', { name: 'Student' }).click();
-  await page.getByRole('link', { name: 'Profile' }).click();
-  // await expect(page.getByRole('region', { name: 'We value your privacy' })).toBeVisible();
+  await page.getByRole('link', { name: 'All Surveys' }).click();
+});
 
+test.only('logout', async ({ page }) => {
+  await page.goto('https://student.michaelkentburns.com/');
+  await expect(page.getByRole('region', { name: 'We value your privacy' })).toBeVisible();
   await page.getByRole('link', { name: 'Student' }).click();
   await page.getByRole('link', { name: 'Logout' }).click();
-
-  // ---------------------
-  await context.close();
-  await browser.close();
-})();
+});
