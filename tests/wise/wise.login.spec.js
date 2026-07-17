9ba3f6acc19753087526df151075d26b0513d8a5
Guillain Wise <guillainwise@gmail.com>
2026-07-17 23:10:02 +0200

    #1: Implement E2E Tests for Login Use Case

---
+++ tests/wise/wise.login.spec.js
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test('loginwise', async ({ page }) => {
+  await page.goto('https://student.michaelkentburns.com/login')
+  await page.getByRole('textbox', { name: 'Username or Email Address' }).click();
+  await page.getByRole('textbox', { name: 'Username or Email Address' }).fill('emsautoservices5@gmail.com');
+  await page.getByRole('textbox', { name: 'Username or Email Address' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('|*Gui_llain@97');
+  await page.getByRole('button', { name: 'Log In' }).click();
+  await page.close();
+})
+
+test('logoutwise', async ({ page }) => {
+  await page.goto('https://student.michaelkentburns.com/login')
+  await page.getByRole('textbox', { name: 'Username or Email Address' }).click();
+  await page.getByRole('textbox', { name: 'Username or Email Address' }).fill('emsautoservices5@gmail.com');
+  await page.getByRole('textbox', { name: 'Username or Email Address' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('|*Gui_llain@97');
+  await page.getByRole('button', { name: 'Log In' }).click();
+  await expect(page.getByRole('region', { name: 'We value your privacy' })).toBeVisible();
+
+  await page.getByRole('link', { name: 'Student' }).click();
+  await page.getByRole('link', { name: 'Logout' }).click();
+
+  await page.close();
+})
