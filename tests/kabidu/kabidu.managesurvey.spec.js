3701af0a9303a1cfbc24f756718e7b9474622afc
Kabidu Munguakonkwa Sage <107588853+Abidusage@users.noreply.github.com>
2026-08-08 06:33:13 +0100

    #4 :Manage Survey (Instructor Mode)

--- tests/kabidu/kabidu.managesurvey.spec.js
+++ tests/kabidu/kabidu.managesurvey.spec.js
@@ -1,33 +1,96 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('kabidu_manage_survey', async ({ page }) => {
-  await page.goto('https://student.michaelkentburns.com/')
-
-  await page.getByRole('link', { name: 'User' }).click();
-  await page.getByRole('link', { name: 'Login' }).click();
-  await page.getByRole('textbox', { name: 'Username or Email Address' }).fill('sage kabidu');
-  await page.getByRole('textbox', { name: 'Username or Email Address' }).press('Tab');
-  await page.getByRole('textbox', { name: 'Password' }).fill('Toujours#toucher*28');
-  await page.getByRole('button', { name: 'Log In' }).click();
-  await page.goto('https://student.michaelkentburns.com/wp-admin/');
-  await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
-  await page.getByRole('link', { name: 'Surveys', exact: true }).click();
-  await expect(page.getByRole('table', { name: 'Table ordered by Date.' })).toBeVisible();
-  await page.locator('#wpbody-content').getByRole('link', { name: 'Add New Survey' }).click();
-  await page.getByRole('textbox', { name: 'Add title' }).fill('abidusage_manage_survey_by_intructor');
-  await page.getByRole('button', { name: 'Publish', exact: true }).click();
-  await page.goto('https://student.michaelkentburns.com/wp-admin/post.php?post=1514&action=edit');
-  await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
-  await page.getByRole('button', { name: 'Edit permalink' }).click();
-  await page.getByRole('textbox', { name: 'URL Slug' }).click();
-  await page.getByRole('textbox', { name: 'URL Slug' }).fill('abidusage_manage_survey_by_intructor_edit');
-  await page.getByRole('button', { name: 'OK' }).click();
-  await expect(page.getByRole('button', { name: 'Edit permalink' })).toBeVisible();
-  await page.getByRole('button', { name: 'Update' }).click();
-  await page.goto('https://student.michaelkentburns.com/wp-admin/post.php?post=1514&action=edit');
-  await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
-  await page.getByRole('link', { name: 'Move to Trash' }).click();
-  await expect(page.getByRole('table', { name: 'Table ordered by Date.' })).toBeVisible();
-  await page.getByRole('menuitem', { name: 'Log Out' }).click();
+test.only("kabidumanagesurvey", async ({ page }) => {
+  await page.goto("https://student.michaelkentburns.com/");
+  await expect(
+    page.getByRole("region", { name: "We value your privacy" }),
+  ).toBeVisible();
+  await page.getByRole("link", { name: "User" }).click();
+  await page.getByRole("link", { name: "Login" }).click();
+  await page
+    .getByRole("textbox", { name: "Username or Email Address" })
+    .fill("sage kabidu");
+  await page.getByRole("textbox", { name: "Password" }).click();
+  await page
+    .getByRole("textbox", { name: "Password" })
+    .fill("Toujours#toucher*28");
+  await page.getByRole("button", { name: "Log In" }).click();
+  await expect(page.getByRole("link", { name: "Dashboard" })).toBeVisible();
+  await page.getByRole("link", { name: "Surveys", exact: true }).click();
+  await expect(
+    page.getByRole("table", { name: "Table ordered by Date." }),
+  ).toBeVisible();
+  await page
+    .locator("#wpbody-content")
+    .getByRole("link", { name: "Add New Survey" })
+    .click();
+  await page
+    .getByRole("textbox", { name: "Add title" })
+    .fill("testing manager survey by sage");
+  await page.locator("#content").click();
+  await page.locator("#content").fill("i am testing the survey");
+  await page.getByRole("textbox", { name: "Description" }).click();
+  await page
+    .getByRole("textbox", { name: "Description" })
+    .fill("description zone");
+  await page.getByRole("textbox", { name: "Start Date" }).fill("2026-08-09");
+  await page.getByRole("textbox", { name: "End Date" }).fill("2026-08-22");
+  await page.getByRole("button", { name: "Publish", exact: true }).click();
+  await expect(page.getByRole("link", { name: "Dashboard" })).toBeVisible();
+  await page.getByRole("link", { name: "Surveys", exact: true }).click();
+  await expect(
+    page.getByRole("table", { name: "Table ordered by Date." }),
+  ).toBeVisible();
+  await page
+    .getByRole("link", { name: "testing manager survey by sage", exact: true })
+    .click();
+  await expect(page.getByRole("link", { name: "Dashboard" })).toBeVisible();
+  await page.getByRole("button", { name: "Edit permalink" }).click();
+  await page
+    .getByRole("textbox", { name: "URL Slug" })
+    .fill("testing-manager-survey-by-sage_edit");
+  await page.getByText("i am testing the survey").click();
+  await page
+    .getByText("i am testing the survey")
+    .fill("i am testing the survey_edit");
+  await page.getByRole("textbox", { name: "Description" }).click();
+  await page
+    .getByRole("textbox", { name: "Description" })
+    .fill("description zone_edit");
+  await page.getByRole("textbox", { name: "Start Date" }).fill("2026-08-27");
+  await page.getByRole("textbox", { name: "End Date" }).fill("2026-09-05");
+  await page.getByRole("button", { name: "OK" }).click();
+  await expect(
+    page.getByRole("button", { name: "Edit permalink" }),
+  ).toBeVisible();
+  await page.getByRole("button", { name: "Update" }).click();
+  await expect(page.getByRole("link", { name: "Dashboard" })).toBeVisible();
+  await page.getByRole("link", { name: "Surveys", exact: true }).click();
+  await expect(
+    page.getByRole("table", { name: "Table ordered by Date." }),
+  ).toBeVisible();
+  await page.getByRole('link', { name: 'testing manager survey by sage', exact: true }).click();
+  await expect(page.getByRole("link", { name: "Dashboard" })).toBeVisible();
+  await page.getByRole("textbox", { name: "Add title" }).click();
+  await page
+    .getByRole("textbox", { name: "Add title" })
+    .fill("testing manager survey by sage_edit");
+  await page.getByRole("button", { name: "Update" }).click();
+  await expect(page.getByRole("link", { name: "Dashboard" })).toBeVisible();
+  await page.getByRole("link", { name: "Surveys", exact: true }).click();
+  await expect(
+    page.getByRole("table", { name: "Table ordered by Date." }),
+  ).toBeVisible();
+  await page
+    .getByRole("link", { name: "Move “testing manager survey" })
+    .click();
+  await expect(
+    page.getByRole("table", { name: "Table ordered by Date." }),
+  ).toBeVisible();
+  await page.getByRole("link", { name: "Surveys", exact: true }).click();
+  await expect(
+    page.getByRole("table", { name: "Table ordered by Date." }),
+  ).toBeVisible();
+  await page.getByRole("menuitem", { name: "Log Out" }).click();
   await page.close();
 });
